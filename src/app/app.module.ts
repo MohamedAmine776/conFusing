f2c181860f3651c6d5e245d6806413e519aa4c37
Mohamed Amine <mohameamine776@gmail.com>
2020-04-21 17:34:21 +0100

    Dialogs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,8 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { PromotionService } from './services/promotion.service';
+import {MatDialogModule} from '@angular/material/dialog';
+import { LoginComponent } from './login/login.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -29,7 +31,8 @@ import { PromotionService } from './services/promotion.service';
     FooterComponent,
     AboutComponent,
     HomeComponent,
-    ContactComponent
+    ContactComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -40,7 +43,12 @@ import { PromotionService } from './services/promotion.service';
     MatListModule,
     MatGridListModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatDialogModule
+  ],
+  
+  entryComponents: [
+    LoginComponent
   ],
   providers: [
     DishService,
--- src/app/services/leaders.service.ts
+++ src/app/services/leaders.service.ts
@@ -11,9 +11,9 @@ leaders = LEADERS;
    return this.leaders;
  }
 
-/* getLeader(id:string): Leader{
+ getLeader(id:string): Leader{
     return this.leaders.filter((leader)=>(leader.id === id))[0];
- }*/
+ }
 getFeaturedLeader():Leader{
   return this.leaders.filter((leader)=> leader.featured)[0];
 }
